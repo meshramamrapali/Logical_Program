2ee40a99f462f0fdfbd7d4af04a6931918764fe0
meshramamrapali <amrapalimeshram86@gmail.com>
2023-03-02 16:01:40 +0530

    ADDED : Temperatur Conversion

--- src/JUnit.java
+++ src/JUnit.java
@@ -1,72 +1,40 @@
 import java.util.Scanner;
 
 public class JUnit {
-
-    public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-
-    boolean keepGoing = true;
-
-     while(keepGoing) {
-        System.out.println("Month");
-        int m = scanner.nextInt();
-        if (m < 1 || m > 12) {
-            System.out.println("Months are between 1 and 12");
-            continue;
-        }
-
-        System.out.println("Day");
-        int d = scanner.nextInt();
-        if (d < 1 || d > 31) {
-            System.out.println("Days are between 1 and 31");
-            continue;
-        }
-
-        System.out.println("Year");
-        int y = scanner.nextInt();
-        if (y < -10000 || y > 10000) {
-            System.out.println("Years are between -10000 and 10000");
-            continue;
-        }
-
-        int y0 = y - (14 - m) / 12;
-        int x = y0 + y0/4 - y0/100 +y0/400;
-        int m0 = m + 12 * ((14 - m) / 12) - 2;
-        int d0 = (d + x + 31 * m0 / 12) % 7;
-        boolean c = 0 <= d0 && d0 <= 6;
-
-        if (c) {
-            String b = "Sunday";
-        } else {
-            if (c) {
-                String b = "Monday";
-            } else {
-                if (c) {
-                    String b = "Tuesday";
-                } else {
-                    if (c) {
-                        String b = "Wednesday";
-                    } else {
-                        if (c) {
-                            String b = "Thursday";
-                        } else {
-                            if (c) {
-                                String b = "Friday";
-                            } else {
-                                if (c) {
-                                    String b = "Saturday";
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        System.out.println("The day of the week is : " + d);
+    static Scanner sc = new Scanner(System.in); // Scanner Class
+    static double C_F(double C){
+        double F = (C * 9/5) + 32;
+        return F;
     }
+    static double F_C(double F){
+        double C = (F - 32) * 5/9;
+        return C;
+    }
+    static double input(String word){
+        System.out.println("Enter "+word+" value:");
+        double val = sc.nextDouble();
+        return val;
+    }
+    static void output(double val, String word){
+        System.out.printf("%s value: %.2f",word,val);
+    }
+    public static void main(String args[]){
+        System.out.println("1. Celcius to Fahrenheit\n2.Fahrenheit to Celcius");
+        do{
+            System.out.println("\nEnter Choice: ");
+            int ch = sc.nextInt();
+            double num = 0;
+            switch(ch){
+                case 1: num = input("Celcius");
+                    output(C_F(num), "Fahrenheit");
+                    break;
 
+                case 2: num = input("Fahrenheit");
+                    output(F_C(num), "Celcius");
+                    break;
 
-
-}
+                default: System.out.println("Invalid Input");
+            }
+        }while(true);
+    }
 }
