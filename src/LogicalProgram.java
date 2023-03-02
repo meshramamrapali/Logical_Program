c39ec4206ce6608708a0ad58e206d1aa02648b96
meshramamrapali <amrapalimeshram86@gmail.com>
2023-03-02 12:45:05 +0530

    ADDED : Reverse Number

--- src/LogicalProgram.java
+++ src/LogicalProgram.java
@@ -1,24 +1,13 @@
 public class LogicalProgram {
     public static void main(String[] args) {
 
-        int n = 11;
-        int count = 0;
+        int num = 123456789, reverse = 0;
 
-        if (n <= 1) {
-            System.out.println("The number is not prime");
-            return;
+        for(;num != 0; num /= 10) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
         }
 
-        for (int i = 1; i <= n / 2; i++) {
-            if (n % i == 0) {
-                count++;
-            }
-        }
-
-        if (count > 1) {
-            System.out.println("The number is not prime");
-        } else {
-            System.out.println("The number is prime");
-        }
+        System.out.println("Reverse Number: " + reverse);
     }
 }
