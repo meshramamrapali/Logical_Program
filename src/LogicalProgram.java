13703f0d8e84cd5d096fea3db896d2ab21dc2087
meshramamrapali <amrapalimeshram86@gmail.com>
2023-03-02 09:36:29 +0530

    ADDED : Fibonacci Series

--- src/LogicalProgram.java
+++ src/LogicalProgram.java
@@ -1,6 +1,17 @@
 public class LogicalProgram {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Logical Program");
+        int i = 1, n = 10, num1 = 0, num2 = 1;
+        System.out.println("Fibonacci Series");
+
+        while (i <= n) {
+            System.out.print(num1 + " ");
+
+            int num3 = num1 + num2;
+            num1 = num2;
+            num2 = num3;
+
+            i++;
+        }
     }
-}
+    }
