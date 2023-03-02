351285aa801515e2203ecd50265d262224168572
meshramamrapali <amrapalimeshram86@gmail.com>
2023-03-02 09:27:27 +0530

    ADDED : Welcome Message

--- src/LogicalProgram.java
+++ src/LogicalProgram.java
@@ -1,5 +1,6 @@
-public class Main {
+public class LogicalProgram {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Welcome to Logical Program");
     }
 }
